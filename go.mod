4e237d6c59c917748b104f4cf2e4351372fb6d26
t1mofey4 <timofeyvalerievich@gmail.com>
2024-03-25 01:38:46 +0300

    :sparkles: added validation of user data before adding to db

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.22.1
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -7,6 +7,68 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUser_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "Valid",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			name: "Empty EMAIL",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "Invalid EMAIL",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "invalid"
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "Invalid password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "Short password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = "123"
+				return u
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+}
+
 func TestUser_BeforeCreate(t *testing.T) {
 	u := model.TestUser(t)
 	assert.NoError(t, u.BeforeCreate())

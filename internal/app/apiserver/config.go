f803b1ebc841a0c341c25380e128b0877157546a
t1mofey4 <timofeyvalerievich@gmail.com>
2024-04-01 19:25:43 +0300

    :recycle: refactored code in the apiserver package

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,12 +1,10 @@
 package apiserver
 
-import "github.com/T1mofey4/restAPI/internal/app/store/sqlstore"
-
 // Config
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_level"`
-	Store    *sqlstore.Config
+	BindAddr    string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
+	DatabaseURL string `toml:"database_url"`
 }
 
 // New Config
@@ -14,6 +12,5 @@ func NewConfig() *Config {
 	return &Config{
 		BindAddr: "8080",
 		LogLevel: "debug",
-		Store:    sqlstore.NewConfig(),
 	}
 }
